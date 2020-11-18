bc68593f81514f47aca0383915de199e81d3ce82
antdp425 <antdp425@aol.com>
2020-11-18 16:43:51 -0500

    adds image + title to blog cards

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-let blogSection = document.querySelector(".blogs")
+let blogSection = document.querySelector(".blog_cards");
 
-fetch("https://dev.to/api/articles?username=antdp425")
+fetch("https://dev.to/api/articles?username=antdp425&per_page=6")
   .then((r) => r.json())
   .then((r) => {
-    r.forEach(b => blogSection.innerHTML += createBlog(b));
+    r.forEach((b) => (blogSection.innerHTML += createBlog(b)));
   });
 
-function createBlog(blog){
-   return `
+function createBlog(blog) {
+   console.log(blog)
+  return `
    <div class="blog_card">
-   I'm a blog card
+      <img src="${blog.social_image}" alt="" class="blog_image">
+      <h2 class="blog_title">
+      ${blog.title}
+      </h2>
    </div>
-   `
+   `;
 }
