d92712eed93dbe2d76ed0654af34e5965bf1e8c0
antdp425 <antdp425@aol.com>
2020-11-18 18:56:09 -0500

    adds additional css for blog cards

--- index.js
+++ index.js
@@ -7,13 +7,15 @@ fetch("https://dev.to/api/articles?username=antdp425&per_page=6")
   });
 
 function createBlog(blog) {
-   console.log(blog)
+  console.log(blog);
   return `
    <div class="blog_card">
+   <a target="_blank" href="${blog.url}">
       <img src="${blog.social_image}" alt="" class="blog_image">
       <h2 class="blog_title">
       ${blog.title}
-      </h2>
+      </h2>          
+      </a>
    </div>
    `;
 }
